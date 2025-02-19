92fce9048214794b18101d5449bf2310129c7cd5
Extroos <abderrahmanchakkouri@gmail.com>
2025-02-19 17:45:20 +0100

    Fix: CSS syntax and mobile sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import store from './store'
 import { AuthProvider } from './providers/AuthProvider'
+import './styles/global.css'
 import './styles/index.css'
 import './styles/loading.css'
+import './styles/auth.css'
+import './components/chat/styles/base.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
